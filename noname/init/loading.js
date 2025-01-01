4c17595c31246958c276a19892af9c6910cffe45
冉龙玉 <yutongg601@gmail.com>
2025-01-01 13:17:27 +0800

    回滚到上一个步骤

--- noname/init/loading.js
+++ noname/init/loading.js
@@ -311,19 +311,7 @@ export async function loadExtension(extension) {
 							lib.skilllist.add(skill);
 						}
 					} else {
-						if (character.img) {
-							if (!character.img.some(str => typeof str == "string" && /^(?:db:extension-.+?|ext|img):.+/.test(str))) {
-								const i = extension[3] ? `db:extension-${extension[0]}:${charaName}.jpg` : `ext:${extension[0]}/${charaName}.jpg`;
-								character.img.add(i);
-							}
-						}
-						if (character.dieAudios) {
-							if (!character.dieAudios.some(str => typeof str == "string" && /^die:.+/.test(str))) {
-								const audio = `die:ext:${extension[0]}/${charaName}.mp3`;
-								character.dieAudios.add(audio);
-							}
-						}
-
+						// TODO 宝贝，有空看一下这里的原画和语音，绕得我头晕晕的。先睡一觉喵
 						if (character.isBoss || character.isHiddenBoss) {
 							lib.config.forbidai.add(charaName);
 						}
