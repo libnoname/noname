4575a62f92040bceb80d2650b7dff03700dcc26c
冉龙玉 <yutongg601@gmail.com>
2025-01-02 20:42:18 +0800

    重新格式化importjs

--- noname/init/import.js
+++ noname/init/import.js
@@ -109,26 +109,23 @@ function generateImportFunction(type, pathParser) {
 }
 
 async function createEmptyExtension(name) {
-	const extensionInfo = await lib.init.promises
-		.json(`${lib.assetURL}extension/${name}/info.json`) //await import(`../../extension/${name}/info.json`,{assert:{type:'json'}})
-		.then(
-			info => info,
-			() => {
-				return {
-					name: name,
-					intro: `扩展<b>《${name}》</b>尚未开启，请开启后查看信息。（建议扩展添加info.json以在关闭时查看信息）`,
-					author: "未知",
-					diskURL: "",
-					forumURL: "",
-					version: "1.0",
-				};
-			}
-		);
+	const extensionInfo = await lib.init.promises.json(`${lib.assetURL}extension/${name}/info.json`) //await import(`../../extension/${name}/info.json`,{assert:{type:'json'}})
+	.then(info => info, () => {
+		return {
+			name,
+			intro: `扩展<b>《${name}》</b>尚未开启，请开启后查看信息。（建议扩展添加info.json以在关闭时查看信息）`,
+			author: "未知",
+			diskURL: "",
+			forumURL: "",
+			version: "1.0",
+		};
+	});
 	return {
 		name: extensionInfo.name,
 		editable: false,
-		content: function (config, pack) {},
-		precontent: function () {},
+		arenaReady () {},
+		content (config, pack) {},
+		precontent () {},
 		config: {},
 		help: {},
 		package: {
