6dfc4f76ac6dc839b547c6e455a6ea1b39141153
冉龙玉 <yutongg601@gmail.com>
2025-01-04 07:49:31 +0800

    同步扩展的更新

--- noname/game/index.js
+++ noname/game/index.js
@@ -2084,10 +2084,10 @@ export class Game extends GameCompatible {
 				content = object.content,
 				precontent = object.precontent;
 			extensionPack.code = {
-				arenaReady: arenaReady,
-				content: content,
-				precontent: precontent,
-				help: help,
+				arenaReady,
+				content,
+				precontent,
+				help,
 				config: objectConfig,
 			};
 			try {
@@ -2098,7 +2098,7 @@ export class Game extends GameCompatible {
 					delete _status.extension;
 				}
 			} catch (e1) {
-				console.log(`加载《${name}》扩展的precontent时出现错误。`, e1);
+				console.error(`加载《${name}》扩展的precontent时出现错误。`, e1);
 				if (!lib.config.extension_alert) alert(`加载《${name}》扩展的precontent时出现错误。\n该错误本身可能并不影响扩展运行。您可以在“设置→通用→无视扩展报错”中关闭此弹窗。\n${decodeURI(e1.stack)}`);
 			}
 
