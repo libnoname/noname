1960e467341f4cee9214749dcf95a5900a409328
IceCola <739201322@qq.com>
2025-08-04 15:41:35 +0800

    不再使用常量PENDING，避免导致内存泄漏喵

--- noname/util/utils.js
+++ noname/util/utils.js
@@ -4,8 +4,6 @@
 // 禁止什么都往里面丢喵！除非这个函数确实规模很小不足以成单文件，而且又确实没有现成的其他文件可以安放喵
 // 第二件事是禁止在这里面丢类喵！类请另行创建文件喵！
 
-const PENDING = new Promise(() => {});
-
 /**
  * 防抖函数喵
  * 
@@ -46,7 +44,7 @@ export function throttle(sourceFunction, delay = 500) {
     // @ts-expect-error 还是TS好用喵
     return function(...args) {
         if (lastTimerId != null) {
-            return PENDING;
+            return new Promise(() => {});
         }
 
         return new Promise(resolve => {
