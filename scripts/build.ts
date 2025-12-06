1184081be8edb3c809ae7707b50aac1089160b25
kuangshen04 <kuangshen04@gmail.com>
2025-12-06 15:58:08 +0800

    更新asset

--- scripts/build.ts
+++ scripts/build.ts
@@ -28,6 +28,7 @@ const staticModules: Target[] = [
 	{ src: "game", dest: "" },
 	{ src: "LICENSE", dest: "" },
 	{ src: "README.md", dest: "" },
+	{ src: ".nomedia", dest: "" },
 	// step 无法编译，需要覆盖
 	// 重构之后请删除
 	{ src: "noname/get/pinyins/index.js", dest: "noname/get/pinyins" },
@@ -39,6 +40,7 @@ const staticModules: Target[] = [
 if (argv.mode) {
 	staticModules.push({ src: "audio", dest: "" });
 	staticModules.push({ src: "image", dest: "" });
+	staticModules.push({ src: "docs", dest: "" });
 	staticModules.push({ src: "extension", dest: "" });
 	staticModules.push(
 		...[
