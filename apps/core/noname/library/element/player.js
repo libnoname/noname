90c086d21c2462ed37f359a10c9feff354598018
rintim <rintim@foxmail.com>
2026-04-12 13:00:23 +0800

    fix: 修复获取攻击距离的函数类型

--- apps/core/noname/library/element/player.js
+++ apps/core/noname/library/element/player.js
@@ -12119,7 +12119,7 @@ export class Player extends HTMLDivElement {
 	}
 	/**
 	 * 返回一些牌的攻击距离
-	 * @param { Card[] } cards
+	 * @param { Card[] } [cards]
 	 * @returns { number }
 	 */
 	getEquipRange(cards) {
