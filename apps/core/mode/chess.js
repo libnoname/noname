c90ded3839c9b788ee1ea3319de87b417a5ee0c5
rintim <rintim@foxmail.com>
2026-06-03 03:32:56 +0800

    perf: 将【统率】的content重写成async content

--- apps/core/mode/chess.js
+++ apps/core/mode/chess.js
@@ -5068,34 +5068,36 @@ export default () => {
 				forced: true,
 				popup: false,
 				priority: 10,
-				content() {
-					for (var i = 0; i < game.data.character.length; i++) {
-						var skills = lib.character[game.data.character[i]][3];
-						var add = false;
-						for (var j = 0; j < skills.length; j++) {
-							var info = lib.skill[skills[j]];
-							if (info.gainable || !info.unique) {
-								add = true;
-								break;
+				async content(event, trigger, player) {
+					const storage = player.storage.tongshuai;
+					for (const name of game.data.character) {
+						const skills = lib.character[name][3];
+						let add = false;
+						for (const skill of skills) {
+							const info = lib.skill[skill];
+							if (!info.gainable && info.unique) {
+								continue;
 							}
+							add = true;
+							break;
 						}
 						if (add) {
-							player.storage.tongshuai.list.push(game.data.character[i]);
+							storage.list.push(name);
 						}
 					}
-					for (var i = 0; i < game.players.length; i++) {
-						player.storage.tongshuai.list.remove([game.players[i].name]);
-						player.storage.tongshuai.list.remove([game.players[i].name1]);
-						player.storage.tongshuai.list.remove([game.players[i].name2]);
+					for (const current of game.players) {
+						storage.list.remove([current.name]);
+						storage.list.remove([current.name1]);
+						storage.list.remove([current.name2]);
 					}
-					player.storage.tongshuai.unowned = player.storage.tongshuai.list.slice(0);
-					player.storage.tongshuai.unowned.sort(lib.sort.random);
-					if (player.storage.tongshuai.unowned.length > 1) {
-						player.storage.tongshuai.get(2);
-					} else if (player.storage.tongshuai.unowned.length == 1) {
-						player.storage.tongshuai.get();
-					} else {
+					storage.unowned = storage.list.slice(0);
+					storage.unowned.sort(lib.sort.random);
+					if (!storage.unowned.length) {
 						player.removeSkill("tongshuai");
+					} else if (storage.unowned.length === 1) {
+						storage.get();
+					} else {
+						storage.get(2);
 					}
 				},
 			},
@@ -5106,7 +5108,7 @@ export default () => {
 					if (!player.hasSkill("tongshuai")) {
 						return false;
 					}
-					if (name == "phaseBegin" && game.phaseNumber == 1) {
+					if (name === "phaseBegin" && game.phaseNumber === 1) {
 						return false;
 					}
 					return true;
@@ -5114,104 +5116,103 @@ export default () => {
 				priority: -9,
 				forced: true,
 				popup: false,
-				content() {
-					var slist = player.storage.tongshuai.owned;
-					var list = [];
-					for (var i in slist) {
-						list.push(i);
+				async content(event, trigger, player) {
+					const slist = player.storage.tongshuai.owned;
+					const list = Object.keys(slist);
+					if (!event.isMine()) {
+						return;
 					}
-					if (event.isMine()) {
-						event.dialog = ui.create.dialog("选择获得一项技能", [list, "character"]);
-						if (trigger.name == "game") {
-							event.control = ui.create.control();
-						} else {
-							event.control = ui.create.control(["cancel"]);
-						}
-						event.clickControl = function (link) {
-							if (link != "cancel") {
-								var currentname = event.dialog.querySelector(".selected.button").link;
-								var mark = player.marks.tongshuai;
-								if (!mark) {
-									player.markSkill("tongshuai");
-									mark = player.marks.tongshuai;
-									if (mark.firstChild) {
-										mark.firstChild.remove();
-									}
-								}
-								mark.setBackground(currentname, "character");
-
-								player.addAdditionalSkill("tongshuai", link);
-								game.addVideo("chess_tongshuai_skill", player, [currentname, link]);
-								player.logSkill("tongshuai2");
-								game.log(player, "获得技能", "【" + get.translation(link) + "】");
-								player.popup(link);
-
-								for (var i = 0; i < event.dialog.buttons.length; i++) {
-									if (event.dialog.buttons[i].classList.contains("selected")) {
-										var name = event.dialog.buttons[i].link;
-										player.sex = lib.character[name][0];
-										player.group = lib.character[name][1];
-										// player.node.identity.style.backgroundColor=get.translation(player.group+'Color');
-										break;
-									}
+					event.dialog = ui.create.dialog("选择获得一项技能", [list, "character"]);
+					if (trigger.name === "game") {
+						event.control = ui.create.control();
+					} else {
+						event.control = ui.create.control(["cancel"]);
+					}
+					event.clickControl = link => {
+						if (link !== "cancel") {
+							const currentname = event.dialog.querySelector(".selected.button").link;
+							let mark = player.marks.tongshuai;
+							if (!mark) {
+								player.markSkill("tongshuai");
+								mark = player.marks.tongshuai;
+								if (mark.firstChild) {
+									mark.firstChild.remove();
 								}
 							}
-							ui.auto.show();
-							event.dialog.close();
-							event.control.close();
-							_status.imchoosing = false;
-							game.resume();
-						};
-						event.control.custom = event.clickControl;
-						ui.auto.hide();
-						_status.imchoosing = true;
-						game.pause();
-						for (var i = 0; i < event.dialog.buttons.length; i++) {
-							event.dialog.buttons[i].classList.add("selectable");
+							mark.setBackground(currentname, "character");
+
+							player.addAdditionalSkill("tongshuai", link);
+							game.addVideo("chess_tongshuai_skill", player, [currentname, link]);
+							player.logSkill("tongshuai2");
+							game.log(player, "获得技能", `【${get.translation(link)}】`);
+							player.popup(link);
+
+							for (const button of event.dialog.buttons) {
+								if (!button.classList.contains("selected")) {
+									continue;
+								}
+								const name = button.link;
+								player.sex = lib.character[name][0];
+								player.group = lib.character[name][1];
+								// player.node.identity.style.backgroundColor=get.translation(player.group+'Color');
+								break;
+							}
 						}
-						event.custom.replace.button = function (button) {
-							if (button.classList.contains("selected")) {
-								button.classList.remove("selected");
-								if (trigger.name == "game") {
-									event.control.style.opacity = 0;
-								} else {
-									event.control.replace(["cancel"]);
-								}
+						ui.auto.show();
+						event.dialog.close();
+						event.control.close();
+						_status.imchoosing = false;
+						game.resume();
+					};
+					event.control.custom = event.clickControl;
+					ui.auto.hide();
+					_status.imchoosing = true;
+					game.pause();
+					for (const button of event.dialog.buttons) {
+						button.classList.add("selectable");
+					}
+					event.custom.replace.button = button => {
+						if (button.classList.contains("selected")) {
+							button.classList.remove("selected");
+							if (trigger.name === "game") {
+								event.control.style.opacity = 0;
 							} else {
-								for (var i = 0; i < event.dialog.buttons.length; i++) {
-									event.dialog.buttons[i].classList.remove("selected");
-								}
-								button.classList.add("selected");
-								event.control.replace(slist[button.link]);
-								if (trigger.name == "game" && getComputedStyle(event.control).opacity == 0) {
-									event.control.style.transition = "opacity 0.5s";
-									ui.refresh(event.control);
-									event.control.style.opacity = 1;
-									event.control.style.transition = "";
-									ui.refresh(event.control);
-								} else {
-									event.control.style.opacity = 1;
-								}
+								event.control.replace(["cancel"]);
 							}
 							event.control.custom = event.clickControl;
-						};
-						event.custom.replace.window = function () {
-							for (var i = 0; i < event.dialog.buttons.length; i++) {
-								if (event.dialog.buttons[i].classList.contains("selected")) {
-									event.dialog.buttons[i].classList.remove("selected");
-									if (trigger.name == "game") {
-										event.control.style.opacity = 0;
-									} else {
-										event.control.replace(["cancel"]);
-									}
-									event.control.custom = event.clickControl;
-									return;
-								}
+							return;
+						}
+						for (const buttonx of event.dialog.buttons) {
+							buttonx.classList.remove("selected");
+						}
+						button.classList.add("selected");
+						event.control.replace(slist[button.link]);
+						if (trigger.name !== "game" || getComputedStyle(event.control).opacity !== "0") {
+							event.control.style.opacity = 1;
+						} else {
+							event.control.style.transition = "opacity 0.5s";
+							ui.refresh(event.control);
+							event.control.style.opacity = 1;
+							event.control.style.transition = "";
+							ui.refresh(event.control);
+						}
+						event.control.custom = event.clickControl;
+					};
+					event.custom.replace.window = () => {
+						for (const button of event.dialog.buttons) {
+							if (!button.classList.contains("selected")) {
+								continue;
 							}
-						};
-					} else {
-						event.finish();
-					}
+							button.classList.remove("selected");
+							if (trigger.name === "game") {
+								event.control.style.opacity = 0;
+							} else {
+								event.control.replace(["cancel"]);
+							}
+							event.control.custom = event.clickControl;
+							return;
+						}
+					};
 				},
 			},
 			tongshuai3: {
@@ -5219,9 +5220,9 @@ export default () => {
 				trigger: { player: "phaseBegin" },
 				forced: true,
 				filter(event, player) {
-					return player.storage.tongshuai && player.storage.tongshuai.unowned && player.storage.tongshuai.unowned.length > 0;
+					return player.storage.tongshuai && player.storage.tongshuai.unowned?.length > 0;
 				},
-				content() {
+				async content(event, trigger, player) {
 					player.storage.tongshuai.get();
 				},
 			},
