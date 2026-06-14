bb22f65a5ffbb9900598a42fbe4de5f4140881a9
Foxissimo-8 <1157235331@qq.com>
2026-06-14 22:47:45 +0800

    添加海外星法正，为加强版

--- apps/core/character/tw/skill.js
+++ apps/core/character/tw/skill.js
@@ -28569,6 +28569,204 @@ const skills = {
 			}
 		},
 	},
+	//TW☆法正
+	cangjia: {
+		audio: 4,
+		audio: "youtan",
+		trigger: {
+			player: "gainAfter",
+			global: "loseAsyncAfter",
+		},
+		filter(event, player, name) {
+			const evt = event.getParent("phaseUse", true);
+			if (evt && evt.player == player) {
+				return false;
+			}
+			const cards = event.getg(player);
+			return cards?.length && cards.some(card => !player.getStorage("cangjia").includes(get.suit(card)));
+		},
+		intro: { content: "已记录花色：$" },
+		forced: true,
+		onremove(player, skill) {
+			player.removeTip(skill);
+			player.setStorage(skill, []);
+		},
+		async content(event, trigger, player) {
+			const suits = trigger
+				.getg(player)
+				.map(card => get.suit(card))
+				.removeArray(player.getStorage(event.name));
+			if (suits?.length) {
+				player.markAuto(event.name, suits);
+				player.addTip(
+					event.name,
+					`忧叹${player
+						.getStorage(event.name)
+						.sort((a, b) => lib.suit.indexOf(b) - lib.suit.indexOf(a))
+						.map(i => get.translation(i))
+						.join("")}`
+				);
+			}
+		},
+		mod: {
+			cardEnabled(card, player) {
+				if (!player.isPhaseUsing() || get.suit(card) == "unsure") {
+					return;
+				}
+				if (!player.getStorage("cangjia").includes(get.suit(card))) {
+					return false;
+				}
+			},
+			cardSavable(card, player) {
+				if (!player.isPhaseUsing() || get.suit(card) == "unsure") {
+					return;
+				}
+				if (!player.getStorage("cangjia").includes(get.suit(card))) {
+					return false;
+				}
+			},
+		},
+	},
+	duohui: {
+		audio: 2,
+		audio: "ciren",
+		global: "duohui_global",
+		subSkill: {
+			global: {
+				trigger: {
+					player: "phaseZhunbeiBegin",
+				},
+				filter(event, player) {
+					if (!player.countCards("he")) {
+						return false;
+					}
+					return game.hasPlayer(current => {
+						return current != player && current.hasSkill("duohui");
+					});
+				},
+				async cost(event, trigger, player) {
+					const targets = game.filterPlayer(current => current != player && current.hasSkill("duohui"));
+					event.result = await player
+						.chooseCardTarget({
+							prompt: get.prompt("duohui"),
+							prompt2: `将一张牌交给${get.translation(targets)}${targets.length > 1 ? "中的一人" : ""}，令其交给你另一张同花色牌，或你摸一张牌`,
+							position: "he",
+							filterCard: true,
+							filterTarget(card, player, target) {
+								return get.event().targetx.includes(target);
+							},
+							targetx: targets,
+							ai1(card) {
+								return 6 - get.value(card);
+							},
+							ai2(target) {
+								const player = get.player();
+								return get.attitude(player, target);
+							},
+						})
+						.forResult();
+					event.result.skill_popup = false;
+				},
+				async content(event, trigger, player) {
+					const {
+							cards,
+							targets: [target],
+						} = event,
+						suit = get.suit(cards[0]);
+					await target.logSkill("duohui", player);
+					await player.give(cards, target);
+					const result = await target
+						.chooseToGive(player, `交给${get.translation(player)}另一张${get.translation(suit)}牌，否则其摸一张牌`, "he")
+						.set("filterCard", card => {
+							const { player, preCards, suit } = get.event();
+							return !preCards.includes(card) && get.suit(card) == suit;
+						})
+						.set("ai", card => {
+							const { player, att } = get.event(),
+								value = get.value(card);
+							if (value <= 0 && att <= 0) {
+								return 10;
+							}
+							if (value > 15 && att > 0) {
+								return 10;
+							}
+							return 0;
+						})
+						.set("att", get.attitude(target, player))
+						.set("preCards", cards)
+						.set("suit", suit)
+						.forResult();
+					if (!result?.bool) {
+						await player.draw();
+					}
+				},
+			},
+		},
+	},
+	yueyuan: {
+		audio: 2,
+		audio: "zhancai",
+		enable: "phaseUse",
+		usable: 1,
+		filter(event, player) {
+			return player.getStorage("cangjia").length;
+		},
+		prompt() {
+			const num = get.player().getStorage("cangjia").length;
+			return `摸${get.cnNumber(num)}张牌，然后移去一个记录的花色`;
+		},
+		manualConfirm: true,
+		async content(event, trigger, player) {
+			const skill = "cangjia";
+			await player.draw(player.getStorage(skill).length);
+			const list = player.getStorage(skill);
+			if (!list?.length) {
+				return;
+			}
+			const result = await player
+				.chooseButton(["展才：移去一个花色", [list.map(suit => ["", "", `lukai_${suit}`]), "vcard"]], true)
+				.set("ai", button => {
+					const player = get.player(),
+						suit = button.link[2].slice(6);
+					const num = player.countCards("hs", card => {
+						return get.suit(card) == suit && player.hasValueTarget(card);
+					});
+					if (num > 0) {
+						return 1 / num;
+					}
+					return 2;
+				})
+				.forResult();
+			if (result?.bool && result.links?.length) {
+				player.unmarkAuto(
+					skill,
+					result.links.map(i => i[2].slice(6))
+				);
+				player.addTip(
+					skill,
+					`忧叹${player
+						.getStorage(skill)
+						.map(i => get.translation(i))
+						.join("")}`
+				);
+			}
+		},
+		ai: {
+			order: 9,
+			combo: "cangjia",
+			result: {
+				player(player) {
+					return player.getStorage("cangjia").filter(suit => {
+						return (
+							player.countCards("hs", card => {
+								return get.suit(card) == suit && player.hasValueTarget(card);
+							}) === 0
+						);
+					}).length;
+				},
+			},
+		},
+	},
 	//英文版特典武将凯撒
 	zhengfu: {
 		trigger: {
