b8fbdab4586a7ae85a9183b6a96fdedbecbea833
qingfeng <3205486470@qq.com>
2026-07-13 21:20:22 +0800

    为game.countGroup增加一个参数group用来获取具体特定势力的角色数，sbxiaoji语音修复

--- apps/core/character/collab/skill.js
+++ apps/core/character/collab/skill.js
@@ -95,7 +95,7 @@ const skills = {
 						return false;
 					}
 					const index = player.getHistory("useCard", evt => get.name(evt.card) == "jiu").indexOf(event);
-					const num = game.countPlayer(current => current.group === "wei");
+					const num = game.countGroup("wei");
 					return index < num && event.addCount !== false;
 				},
 				async content(event, trigger, player) {
--- apps/core/noname/game/index.js
+++ apps/core/noname/game/index.js
@@ -10287,14 +10287,20 @@ ${e instanceof Error ? e.stack : String(e)}`);
 			return func(value, lib.card[value]);
 		});
 	}
-	countGroup() {
+	countGroup(group) {
 		const list = lib.group.slice(0);
 		return game.countPlayer(current => {
 			if (!list.includes(current.group)) {
 				return false;
 			}
-			list.remove(current.group);
-			return true;
+			if (group) {
+				if(group === current.group) {
+					return true;
+				}
+			} else {
+				list.remove(current.group);
+				return true;
+			}
 		});
 	}
 	/**
