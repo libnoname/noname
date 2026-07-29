3e6a9bcd2ecce9fc018593dc50dcd45a2dbc8b1c
qingfeng <3205486470@qq.com>
2026-07-29 16:35:03 +0800

    Update skill.js

--- apps/core/character/xianding/skill.js
+++ apps/core/character/xianding/skill.js
@@ -188,7 +188,7 @@ const skills = {
 				target(card, player, target) {
 					if (_status._dcxianfu_check) return;
 					const mingjie = target.getStorage("dcmingjie_effect");
-					if (!Array.isArray(mingjie) || !mingjie?.isIn() || !Array.isArray(lib.phaseName)) return;
+					if (Array.isArray(mingjie) || !mingjie?.isIn() || !Array.isArray(lib.phaseName)) return;
 					if (!lib.phaseName.some(item => !["phaseZhunbei", "phaseJieshu"].includes(item) && !target.skipList.includes(item))) return;
 					_status._dcxianfu_check = true;
 					const eff = get.effect(target, card, player, target);
