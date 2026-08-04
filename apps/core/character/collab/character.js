be30d5f0d51e041817dea2986e0bbda761ceb6e5
qingfeng <3205486470@qq.com>
2026-08-04 09:35:37 +0800

    回退ol牛马武将及技能代码
    等后面有时间了再改   先把pr合了再说

--- apps/core/character/collab/character.js
+++ apps/core/character/collab/character.js
@@ -1,11 +1,4 @@
 const characters = {
-	ol_niuma: {
-		sex: "male",
-		group: "shen",
-		hp: 4,
-		skills: ["oldianbu", "oljuhun"],
-		names: "null|null-null|null",
-	},
 	natu_guanyu: {
 		sex: "male",
 		group: "shu",
