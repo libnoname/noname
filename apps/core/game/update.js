da788d3a6ee05c091448e4a4f408dc63051970b1
rintim <rintim@foxmail.com>
2026-08-14 17:50:44 +0800

    chore: 更新版本号

--- apps/core/game/update.js
+++ apps/core/game/update.js
@@ -1,9 +1,9 @@
 window.noname_update = {
-	version: "1.11.5",
+	version: "1.11.5.1",
 	update: "NULL",
 	changeLog: [
-		"？",
-		"不知道写啥，总之新增多个武将",
+		"BUG复BUG，BUG何其多",
+		"修复了若干BUG，也只有修复了若干个BUG",
 		"详细信息请参考game/updateLog.md"
 	],
 	files: [],
