acf51f0b5b2d8caebaa07cde10550b4de187d14f
rintim <rintim@foxmail.com>
2026-08-15 23:40:39 +0800

    feat: 增加只break的release分支，用于表示release分支下省略显示标签

--- apps/core/noname/ui/create/menu/pages/otherMenu.js
+++ apps/core/noname/ui/create/menu/pages/otherMenu.js
@@ -109,6 +109,8 @@ export const otherMenu = function (/** @type { boolean | undefined } */ connectM
 			case "nightly":
 				buildLabel = `nightly ${builtAt?.slice(0, 10) || "unknown"}`;
 				break;
+			case "release":
+				break;
 		}
 		li1.textContent = `游戏版本：${lib.version}${buildLabel ? ` (${buildLabel})` : ""}`;
 		li3.innerHTML = '由于无名杀正在重构项目结构，在线更新暂时无法使用，请访问github主页获取最新版本。';
