1da0e9c498c0ffc0b8153d24f1ca14920fe0935f
rintim <rintim@foxmail.com>
2026-08-15 15:10:36 +0800

    fix: 修复【持剑】视为拥有的青釭剑不提供攻击范围

--- apps/core/character/huicui/skill.js
+++ apps/core/character/huicui/skill.js
@@ -19392,7 +19392,7 @@ const skills = {
 			qinggang: {
 				mod: {
 					attackRangeBase(player) {
-						const num = lib.card?.guanshi?.distance?.qinggang;
+						const num = lib.card.qinggang.distance.attackFrom;
 						if (typeof num != "number" || !player.hasEmptySlot(1)) {
 							return;
 						}
