18395179cea6274de0947395f03101294767f938
ziyoo <166354372+zziyoo@users.noreply.github.com>
2026-08-19 22:59:53 +0800

    1

--- apps/core/character/mobile/skill.js
+++ apps/core/character/mobile/skill.js
@@ -1,4 +1,4 @@
-import { lib, game, ui, get, ai, _status } from "noname";
+﻿import { lib, game, ui, get, ai, _status } from "noname";
 
 /** @type { importCharacterConfig["skill"] } */
 const skills = {
