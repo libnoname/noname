5b644655040f52c69c20cbe050c63aa402ef2b6b
qingfeng <3205486470@qq.com>
2026-08-27 14:30:19 +0800

    糜夫人清玉加上锁定技标签

--- apps/core/character/shiji/skill.js
+++ apps/core/character/shiji/skill.js
@@ -6570,7 +6570,7 @@ const skills = {
 	qingyu: {
 		audio: 3,
 		dutySkill: true,
-		locked: false,
+		forced: true,
 		group: ["qingyu_achieve", "qingyu_fail", "qingyu_defend"],
 		subSkill: {
 			defend: {
