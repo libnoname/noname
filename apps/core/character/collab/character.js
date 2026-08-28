bd5fec9f42bffc463d7535b847eedc08c2dfae8f
qingfeng <3205486470@qq.com>
2026-08-28 19:43:16 +0800

    Update character.js

--- apps/core/character/collab/character.js
+++ apps/core/character/collab/character.js
@@ -342,7 +342,7 @@ const characters = {
 	ol_jsrg_lvbu: {
 		sex: "male",
 		group: "qun",
-		hp: 4,
+		hp: 5,
 		skills: ["olfengzhu", "olyuyu", "ollbzhiji", "oljiejiu"],
 		img: "image/character/jsrg_lvbu.jpg",
 		names: "吕|布",
