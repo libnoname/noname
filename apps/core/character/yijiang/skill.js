82277a2ccdbfad3bd634cffec5a9ff27f8443ce1
qingfeng <3205486470@qq.com>
2026-08-30 15:57:18 +0800

    Revert "Update skill.js"
    This reverts commit be5e208581c103ec4a3ac661fcd64f2305140631.

--- apps/core/character/yijiang/skill.js
+++ apps/core/character/yijiang/skill.js
@@ -11532,7 +11532,7 @@ const skills = {
 			player.awakenSkill(event.name);
 			await player.loseMaxHp();
 			await player.chooseDrawRecover(2, true, (event, player) => {
-				if (player.hp == 1 && player.getDamagedHp() > 1) {
+				if (player.hp == 1 && player.isDamaged()) {
 					return "recover_hp";
 				}
 				return "draw_card";
