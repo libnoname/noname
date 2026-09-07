0f1f1882bcb359a50e1172a21a08399c88959fcd
灰风 <133578152+YYUZDS@users.noreply.github.com>
2026-09-07 18:19:00 +0800

    fix: 修改【百变】为非锁定技 (#4399)
    rt

--- apps/core/character/collab/skill.js
+++ apps/core/character/collab/skill.js
@@ -1635,6 +1635,7 @@ const skills = {
 			player: ["damageEnd", "phaseBegin"],
 		},
 		forced: true,
+		locked: false,
 		skillList: ["dcshentui", "dcxurui", "dchaoshi", "dcniyun", "dcfanzhuan", "dckeshui", "dchuibian", "dcweiqu", "dcmaimeng", "dczuandai", "dcgunyuan", "quanjia", "xuyuan", "xiaomian"],
 		filter(event, player) {
 			return get.info("dcbaibian").skillList.some(skill => !player.hasSkill(skill, null, false, false));
