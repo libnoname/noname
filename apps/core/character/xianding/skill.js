841990809e4488b0308219f0dc9f408a150c0d3a
qingfeng <3205486470@qq.com>
2026-09-07 18:01:04 +0800

    Update skill.js

--- apps/core/character/xianding/skill.js
+++ apps/core/character/xianding/skill.js
@@ -3051,7 +3051,6 @@ const skills = {
 					return [get.type2(event.card), get.suit(event.card)].some(link => player.getStorage("dcsbjingmou_mark").includes(link));
 				},
 				async content(event, trigger, player) {
-					player.changeZhuanhuanji("dcsbjingmou");
 					const links = [get.type2(trigger.card), get.suit(trigger.card)].filter(link => player.getStorage(event.name).includes(link));
 					player.unmarkAuto(event.name, links);
 					player.addSkill("dcsbjingmou_mark2");
@@ -3062,7 +3061,7 @@ const skills = {
 					const {
 						targets: [target],
 					} = event;
-					if (player.storage.dcsbjingmou) {
+					if (!player.storage.dcsbjingmou) {
 						const result = await player
 							.chooseBool({
 								prompt: get.prompt(event.name, target),
@@ -3129,6 +3128,7 @@ const skills = {
 								}
 							});
 					}
+					player.changeZhuanhuanji("dcsbjingmou");
 				},
 			},
 			change: {
