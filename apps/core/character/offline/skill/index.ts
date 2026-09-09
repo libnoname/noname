976d5efb0e568c3e7babbcd86af89b1d0126da2c
rintim <rintim@foxmail.com>
2026-09-09 23:03:15 +0800

    fix: 修复线下包技能因vite神力使用不当失效

--- apps/core/character/offline/skill/index.ts
+++ apps/core/character/offline/skill/index.ts
@@ -1,5 +1,5 @@
 const modules = import.meta.glob(["./*.js", "!./index.js"], { eager: true });
 
-const skills = Object.assign({}, ...Object.values(modules));
+const skills = Object.assign({}, ...Object.values(modules).map((module: any) => module.default));
 
 export default skills;
