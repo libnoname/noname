b33d199a17b5dc68a18d30c75ff589c2932925c6
qingfeng <3205486470@qq.com>
2026-09-10 10:34:42 +0800

    马邈增强（大陈圭）

--- apps/core/character/huicui/skill.js
+++ apps/core/character/huicui/skill.js
@@ -1013,8 +1013,16 @@ const skills = {
 					.forResult();
 			} else {
 				event.result = await player
-					.chooseBool(get.prompt2(event.skill))
-					//.set("choice", player.isDamaged() || num < -1 || get.attitude(player, trigger.player) > 0)
+					.chooseBool({
+						prompt: get.prompt2(event.skill),
+						ai() {
+							const player = get.player(),
+								target = _status.currentPhase;
+							if (player.isDamaged()) return true;
+							if (target?.isIn() && get.attitude(player, target) < 0) return true;
+							return false;
+						},
+					})
 					.forResult();
 			}
 		},
@@ -1027,13 +1035,12 @@ const skills = {
 				await player.modedDiscard(event.cards);
 			}
 			const num = player.countCards("h") - count;
-			if (num == 0) {
+			if (num <= 0) {
 				await player.recover();
-			}
-			if (num < 0) {
 				const target = _status.currentPhase;
-				if (target?.isIn() && target.hasGainableCards(player, "he") && player != target) {
-					await player.gainPlayerCard(target, "he", true);
+				const position = player == target ? "e" : "he";
+				if (target?.isIn() && target.hasGainableCards(player, position)) {
+					await player.gainPlayerCard({ target, position, forced: true });
 				}
 			}
 			//player.addTempSkill("dczhangguan_effect");
