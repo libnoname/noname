9c7ef2c54ac9b848091ae275bcb01067349682cd
rintim <rintim@foxmail.com>
2026-09-14 16:19:54 +0800

    perf: 重写外服武将包中【宠齐】的step content

--- apps/core/character/tw/skill.js
+++ apps/core/character/tw/skill.js
@@ -24581,37 +24581,39 @@ const skills = {
 		},
 		forced: true,
 		filter(event, player) {
-			return event.name != "phase" || game.phaseNumber == 0;
+			return event.name !== "phase" || game.phaseNumber === 0;
 		},
 		logTarget: () => game.filterPlayer().sortBySeat(),
-		content() {
-			"step 0";
-			game.filterPlayer()
-				.sortBySeat()
-				.forEach(function (current) {
-					current.addSkills("twfeifu");
-				});
-			//game.log(player,'令所有其他角色获得了技能','#g【非服】')
-			game.delayx();
-			"step 1";
-			player.chooseTarget("是否减1点体力上限，并令一名其他角色获得技能【复纂】？", lib.filter.notMe).set("ai", function (target) {
-				var player = _status.event.player;
-				if (player.hasUnknown() && !target.isZhu) {
-					return 0;
-				}
-				if (player.getEnemies().includes(target)) {
-					return 0;
-				}
-				return get.attitude(player, target);
-			});
-			"step 2";
-			if (result.bool) {
-				player.loseMaxHp();
-				var target = result.targets[0];
-				player.line(target, "fire");
-				target.addSkills("twfuzuan");
-				game.delayx();
+		async content(event, trigger, player) {
+			for (const current of game.filterPlayer().sortBySeat()) {
+				await current.addSkills("twfeifu");
 			}
+			//game.log(player,'令所有其他角色获得了技能','#g【非服】')
+			await game.delayx();
+			const result = await player
+				.chooseTarget({
+					prompt: "是否减1点体力上限，并令一名其他角色获得技能【复纂】？",
+					filterTarget: lib.filter.notMe,
+					ai(target) {
+						if (player.hasUnknown() && !target.isZhu) {
+							return 0;
+						}
+						if (player.getEnemies().includes(target)) {
+							return 0;
+						}
+						return get.attitude(player, target);
+					},
+				})
+				.forResult();
+			if (!result.bool) {
+				return;
+			}
+
+			await player.loseMaxHp();
+			const target = result.targets[0];
+			player.line(target, "fire");
+			await target.addSkills("twfuzuan");
+			await game.delayx();
 		},
 		derivation: ["twfeifu", "twfuzuan"],
 	},
