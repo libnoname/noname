673a6fcfe4410cac7401b7fda327fedd4598d5a9
ziyoo <166354372+zziyoo@users.noreply.github.com>
2026-09-15 22:24:15 +0800

    fix: 修复星语遗留的bug

--- apps/core/noname/library/element/player.js
+++ apps/core/noname/library/element/player.js
@@ -12693,16 +12693,18 @@ export class Player extends HTMLDivElement {
 	getEquipRange(cards) {
 		const player = this;
 		if (!cards) {
-			cards = player.getVCards("e", function (card) {
+			const filter = function (card) {
 				return !card.cards?.some(card => {
 					return ui.selected.cards?.includes(card);
 				});
-			});
+			};
+			cards = player.getVCards("e", filter);
+			cards.push(...player.getCards("e", card => !card.cardSymbol && filter(card)));
 		}
 		const range = cards.reduce((range, card) => {
 			let newRange = false;
 			const info = get.info(card, false);
-			if (info.distance) {
+			if (info?.distance) {
 				//如果存在attackRange 则通过attackRange动态获取攻击范围
 				if (typeof info.distance.attackRange == "function") {
 					newRange = info.distance.attackRange(card, player);
