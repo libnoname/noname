12f8ee6b2833735537f9b0636246f304b22e07af
ziyoo <166354372+zziyoo@users.noreply.github.com>
2026-09-15 22:35:50 +0800

    fix: 修复星语遗留bug

--- apps/core/noname/get/index.js
+++ apps/core/noname/get/index.js
@@ -5877,7 +5877,9 @@ else if (entry[1] !== void 0) stringifying[key] = JSON.stringify(entry[1]);*/
 								var dist = lib.card[name].distance;
 								if (dist.attackFrom) {
 									added = true;
-									const range = get.owner(node) ? get.owner(node).getEquipRange([node[node.cardSymbol]]) : -dist.attackFrom + 1;
+									const rangeCard = Vcard || trueCard[trueCard.cardSymbol] || (!trueCard.classList.contains("emptyequip") && lib.card[trueCard.name] ? trueCard : false);
+									const owner = get.owner(node);
+									const range = owner && rangeCard ? owner.getEquipRange([rangeCard]) : -dist.attackFrom + 1;
 									uiintro.add('<div class="text center">攻击范围：' + range + "</div>"); //
 								}
 							}
