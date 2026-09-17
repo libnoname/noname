1e9d6b3ffd1d3209f56647da8c7c9a31758fa4c8
qingfeng <3205486470@qq.com>
2026-09-17 16:54:05 +0800

    tw疑刘备乘变

--- apps/core/character/tw/skill.js
+++ apps/core/character/tw/skill.js
@@ -30144,12 +30144,12 @@ const skills = {
 			player: ["phaseZhunbeiBegin", "phaseJieshuBegin"],
 		},
 		filter(event, player) {
-			return game.hasPlayer(current => player.canCompare(current));
+			return game.hasPlayer(current => player.canCompare(current, true));
 		},
 		async cost(event, trigger, player) {
 			event.result = await player
 				.chooseTarget(get.prompt2(event.skill), (card, player2, target) => {
-					return player2.canCompare(target);
+					return player2.canCompare(target, true);
 				})
 				.set("ai", target => {
 					const player2 = get.player();
@@ -30160,6 +30160,7 @@ const skills = {
 		async content(event, trigger, player) {
 			await player.draw(1);
 			const target = event.targets[0];
+			if (!player.canCompare(target)) return;
 			const next = player.chooseToCompare(target).set("isDelay", true);
 			await next;
 			await game.delay();
