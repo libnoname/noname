4cbc57af805b2dfbaf4ed8df91c4a5a74a510954
rintim <rintim@foxmail.com>
2026-09-22 00:31:56 +0800

    refactor(ui): 重构游戏名词帮助组件
    - 新增 GamePoptip Vue 组件
    - 渲染规则类游戏术语提示列表
    - 禁用组件持久化

---
+++ apps/core/noname/library/help/GamePoptip.vue
@@ -0,0 +1,19 @@
+<template>
+	<ul class="game-poptip-list">
+		<li v-for="item in items" :key="item.id">
+			<strong>{{ item.name }}</strong>：<br>
+			<!-- 解释为游戏内 HTML 文案，不是用户输入 -->
+			<span v-html="item.info"></span>
+		</li>
+	</ul>
+</template>
+
+<script setup lang="ts">
+import { lib } from "noname";
+defineOptions({ persist: false });
+const items = lib.poptip.getIdList("rule").map(id => ({
+	id,
+	name: lib.poptip.getName(id),
+	info: lib.poptip.getInfo(id),
+}));
+</script>
